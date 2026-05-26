453f16904f08e6fb1a0b7af44c40d70deb2fb154
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-05-26 14:16:23 +0200

    metodo aumentargas

--- src/Coche.java
+++ src/Coche.java
@@ -8,25 +8,32 @@ public class Coche {
     String matricula;
     String modelo;
     Integer velocidad;
+    public int gasolina;
 
     /**
      * Constructor
      * @param modelo
      * @param matricula
      */
-    public Coche(String modelo, String matricula) {
+    public Coche(String modelo, String matricula) { //para simplioficar no pido gasolina a la hora de instanciar Coche y la inicializo a 0
         this.modelo = modelo;
         this.matricula = matricula;
         this.velocidad = 0;
+        this.gasolina = 0; //inicializa gasolina a 0
     }
 
     /**
      * Métod0 aumentarVelocidad que aumenta la velocidad del coche
-     * @param velocidad
+     * @param vel nueva int
      * @return velocidad int
      */
-    public int aumentarVel(int velocidad){
-        this.velocidad+=velocidad;
-        return velocidad;
+    public int aumentarVel(int vel){
+        this.velocidad+=vel;
+        return this.velocidad;
+    }
+
+    public int aumentarGas(int gas){
+        this.gasolina += gas;
+        return this.gasolina;
     }
 }
